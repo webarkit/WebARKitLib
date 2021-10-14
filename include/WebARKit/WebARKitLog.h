53e06b32506c1075d8978c9818c17bc7bebae7a7
kalwalt <info@kalwaltart.it>
2021-10-14 15:29:47 +0200

    first WebARKitLogi methods

---
+++ include/WebARKit/WebARKitLog.h
@@ -0,0 +1,17 @@
+#ifndef WEBARKIT_LOG_H
+#define WEBARKIT_LOG_H
+
+#include <emscripten.h>
+
+void webarkitLOGi(char* message);
+
+void webarkitLOGi(char* message, double * format);
+
+void webarkitLOGi(char* message, float * format);
+
+void webarkitLOGi(char* message, char * format);
+
+void webarkitLOGi(char* message, int  format);
+
+
+#endif // #ifndef WEBARKIT_LOG_H
