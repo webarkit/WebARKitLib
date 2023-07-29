ad8a01f1a5a869e2b261fdb8ada97d2ae62c0ba4
Walter Perdan <github@kalwaltart.it>
2023-07-29 16:06:25 +0200

    almost done for a first test

--- tests/hello_test.cc
+++ tests/hello_test.cc
@@ -3,7 +3,7 @@
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
-  WebARKitManager manager;
+  webarkit::WebARKitManager manager;
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
