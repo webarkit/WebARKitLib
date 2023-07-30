d55003639dcadd46b7665736f3c1c962735f4bd5
Walter Perdan <github@kalwaltart.it>
2023-07-30 22:51:11 +0200

    now test starts but there is a bug to fix

--- tests/hello_test.cc
+++ tests/hello_test.cc
@@ -4,8 +4,11 @@
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
   webarkit::WebARKitManager manager;
+  manager.initialiseBase(webarkit::TRACKER_TYPE::AKAZE_TRACKER);
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
+
+  EXPECT_STREQ(manager.getWebARKitVersion(), "1.0.0");
 }
