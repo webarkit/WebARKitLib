531b2e9b71e10f1533ab5e83eac7fc23fa9cf949
kalwalt <github@kalwaltart.it>
2023-11-04 22:12:49 +0100

    improved classes and features - WebARKitTrackingInfo became a class: - new invertPose, setScale, and getScale method for the scale of the pattern - m_camMatrix as Matx33d

--- WebARKit/WebARKitPattern.cpp
+++ WebARKit/WebARKitPattern.cpp
@@ -1,9 +1,14 @@
 #include <WebARKitPattern.h>
-#include <iostream>
 #include <opencv2/calib3d.hpp>
 
-void WebARKitPatternTrackingInfo::computePose(std::vector<cv::Point3f>& treeDPoints, std::vector<cv::Point2f>& imgPoints,
-                                              cv::Mat& caMatrix, cv::Mat& distCoeffs) {
+WebARKitPatternTrackingInfo::WebARKitPatternTrackingInfo() {
+    pose3d = cv::Mat::zeros(3, 4, CV_64FC1);
+    m_scale = 1.0f;
+}
+
+void WebARKitPatternTrackingInfo::computePose(std::vector<cv::Point3f>& treeDPoints,
+                                              std::vector<cv::Point2f>& imgPoints, cv::Mat& caMatrix,
+                                              cv::Mat& distCoeffs) {
     cv::Mat rvec = cv::Mat::zeros(3, 1, CV_64FC1); // output rotation vector
     cv::Mat tvec = cv::Mat::zeros(3, 1, CV_64FC1); // output translation vector
 
@@ -13,5 +18,18 @@ void WebARKitPatternTrackingInfo::computePose(std::vector<cv::Point3f>& treeDPoi
     cv::Rodrigues(rvec, rMat);
     cv::hconcat(rMat, tvec, pose3d);
 
-    std::cout << "pose3d: " << pose3d.rows << " x " << pose3d.cols << std::endl;
+    invertPose();
+}
+
+void WebARKitPatternTrackingInfo::invertPose() {
+
+    cv::Mat invertPose(3, 4, CV_64FC1);
+    for (auto j = 0; j < 3; j++) {
+        invertPose.at<double>(j, 0) = pose3d.at<double>(j, 0);
+        invertPose.at<double>(j, 1) = -pose3d.at<double>(j, 1);
+        invertPose.at<double>(j, 2) = -pose3d.at<double>(j, 2);
+        invertPose.at<double>(j, 3) = pose3d.at<double>(j, 3) * m_scale * 0.001f * 1.64f;
+    }
+
+    pose3d = invertPose;
 }
