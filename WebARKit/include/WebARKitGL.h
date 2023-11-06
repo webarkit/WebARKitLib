e3997cba627425b169f00ea8a8356a85b1f9e3fc
kalwalt <github@kalwaltart.it>
2023-11-06 15:20:48 +0100

    values and params should be double

--- WebARKit/include/WebARKitGL.h
+++ WebARKit/include/WebARKitGL.h
@@ -27,19 +27,19 @@ void arglCameraViewRHf(float para[3][4], float m_modelview[16], const float scal
     }
 }
 
-void arglCameraViewRHf(cv::Mat para, std::array<float, 16>& m_modelview, const float scale) {
-    m_modelview[0 + 0 * 4] = para.at(0, 0); // R1C1
-    m_modelview[0 + 1 * 4] = para.at(0, 1); // R1C2
-    m_modelview[0 + 2 * 4] = para.at(0, 2);
-    m_modelview[0 + 3 * 4] = para.at(0, 3);
-    m_modelview[1 + 0 * 4] = -para.at(1, 0); // R2
-    m_modelview[1 + 1 * 4] = -para.at(1, 1);
-    m_modelview[1 + 2 * 4] = -para.at(1, 2);
-    m_modelview[1 + 3 * 4] = -para.at(1, 3);
-    m_modelview[2 + 0 * 4] = -para.at(2, 0); // R3
-    m_modelview[2 + 1 * 4] = -para.at(2, 1);
-    m_modelview[2 + 2 * 4] = -para.at(2, 2);
-    m_modelview[2 + 3 * 4] = -para.at(2, 3);
+void arglCameraViewRHf(cv::Mat para, std::array<double, 16>& m_modelview, const double scale) {
+    m_modelview[0 + 0 * 4] = para<double>.at(0, 0); // R1C1
+    m_modelview[0 + 1 * 4] = para<double>.at(0, 1); // R1C2
+    m_modelview[0 + 2 * 4] = para<double>.at(0, 2);
+    m_modelview[0 + 3 * 4] = para<double>.at(0, 3);
+    m_modelview[1 + 0 * 4] = -para<double>.at(1, 0); // R2
+    m_modelview[1 + 1 * 4] = -para<double>.at(1, 1);
+    m_modelview[1 + 2 * 4] = -para<double>.at(1, 2);
+    m_modelview[1 + 3 * 4] = -para<double>.at(1, 3);
+    m_modelview[2 + 0 * 4] = -para<double>.at(2, 0); // R3
+    m_modelview[2 + 1 * 4] = -para<double>.at(2, 1);
+    m_modelview[2 + 2 * 4] = -para<double>.at(2, 2);
+    m_modelview[2 + 3 * 4] = -para<double>.at(2, 3);
     m_modelview[3 + 0 * 4] = 0.0f;
     m_modelview[3 + 1 * 4] = 0.0f;
     m_modelview[3 + 2 * 4] = 0.0f;
