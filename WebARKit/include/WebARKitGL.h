ad15a7f19e4018cad9ea3f751411d0f7cb574cf5
kalwalt <github@kalwaltart.it>
2023-11-06 15:56:12 +0100

    make GL mfunctions part of ebarkit namespace

--- WebARKit/include/WebARKitGL.h
+++ WebARKit/include/WebARKitGL.h
@@ -1,7 +1,9 @@
-#ifndef WEBARKITGL_H
-#define WEBARKITGL_H
+#ifndef WEBARKIT_GL_H
+#define WEBARKIT_GL_H
 
 #include <opencv2/core.hpp>
+
+namespace webarkit {
 
 void arglCameraViewRHf(float para[3][4], float m_modelview[16], const float scale) {
     m_modelview[0 + 0 * 4] = para[0][0]; // R1C1
@@ -28,18 +30,18 @@ void arglCameraViewRHf(float para[3][4], float m_modelview[16], const float scal
 }
 
 void arglCameraViewRHf(cv::Mat para, std::array<double, 16>& m_modelview, const double scale) {
-    m_modelview[0 + 0 * 4] = para<double>.at(0, 0); // R1C1
-    m_modelview[0 + 1 * 4] = para<double>.at(0, 1); // R1C2
-    m_modelview[0 + 2 * 4] = para<double>.at(0, 2);
-    m_modelview[0 + 3 * 4] = para<double>.at(0, 3);
-    m_modelview[1 + 0 * 4] = -para<double>.at(1, 0); // R2
-    m_modelview[1 + 1 * 4] = -para<double>.at(1, 1);
-    m_modelview[1 + 2 * 4] = -para<double>.at(1, 2);
-    m_modelview[1 + 3 * 4] = -para<double>.at(1, 3);
-    m_modelview[2 + 0 * 4] = -para<double>.at(2, 0); // R3
-    m_modelview[2 + 1 * 4] = -para<double>.at(2, 1);
-    m_modelview[2 + 2 * 4] = -para<double>.at(2, 2);
-    m_modelview[2 + 3 * 4] = -para<double>.at(2, 3);
+    m_modelview[0 + 0 * 4] = para.at<double>(0, 0); // R1C1
+    m_modelview[0 + 1 * 4] = para.at<double>(0, 1); // R1C2
+    m_modelview[0 + 2 * 4] = para.at<double>(0, 2);
+    m_modelview[0 + 3 * 4] = para.at<double>(0, 3);
+    m_modelview[1 + 0 * 4] = -para.at<double>(1, 0); // R2
+    m_modelview[1 + 1 * 4] = -para.at<double>(1, 1);
+    m_modelview[1 + 2 * 4] = -para.at<double>(1, 2);
+    m_modelview[1 + 3 * 4] = -para.at<double>(1, 3);
+    m_modelview[2 + 0 * 4] = -para.at<double>(2, 0); // R3
+    m_modelview[2 + 1 * 4] = -para.at<double>(2, 1);
+    m_modelview[2 + 2 * 4] = -para.at<double>(2, 2);
+    m_modelview[2 + 3 * 4] = -para.at<double>(2, 3);
     m_modelview[3 + 0 * 4] = 0.0f;
     m_modelview[3 + 1 * 4] = 0.0f;
     m_modelview[3 + 2 * 4] = 0.0f;
@@ -50,5 +52,6 @@ void arglCameraViewRHf(cv::Mat para, std::array<double, 16>& m_modelview, const 
         m_modelview[14] *= scale;
     }
 }
+} // namespace webarkit
 
 #endif // WEBARKITGL_H
