b6b113c0f790cecc03c3d239ba8ca6baba72b9b9
kalwalt <github@kalwaltart.it>
2023-11-06 17:57:49 +0100

    new cameraProjectionMatrix with gtest

--- WebARKit/include/WebARKitGL.h
+++ WebARKit/include/WebARKitGL.h
@@ -8,6 +8,8 @@ namespace webarkit {
 void arglCameraViewRHf(float para[3][4], float m_modelview[16], const float scale);
 
 void arglCameraViewRHf(cv::Mat para, std::array<double, 16>& m_modelview, const double scale);
+
+void cameraProjectionMatrix(const std::array<double, 9>& calibration, double nearPlane, double farPlane, int screenWidth, int screenHeight, std::array<double, 16>& projectionMatrix);
 } // namespace webarkit
 
 #endif // WEBARKITGL_H
