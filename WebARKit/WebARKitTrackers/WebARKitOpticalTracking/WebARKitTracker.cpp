f4ea9ff70d17f922799dfffb177af8fd1c283ab6
Walter Perdan <github@kalwaltart.it>
2024-03-31 18:51:04 +0200

    fix for getMatches function

--- WebARKit/WebARKitTrackers/WebARKitOpticalTracking/WebARKitTracker.cpp
+++ WebARKit/WebARKitTrackers/WebARKitOpticalTracking/WebARKitTracker.cpp
@@ -309,14 +309,13 @@ class WebARKitTracker::WebARKitTrackerImpl {
         std::vector<std::vector<cv::DMatch>> knnMatches;
         _matcher->knnMatch(frameDescr, refDescr, knnMatches, 2);
 
-        framePts.clear();
-        std::vector<cv::Point2f> refPts;
+        framePoints.clear();
 
         // find the best matches
         for (size_t i = 0; i < knnMatches.size(); ++i) {
             if (knnMatches[i][0].distance < _nn_match_ratio * knnMatches[i][1].distance) {
-                framePts.push_back(frameKeyPts[knnMatches[i][0].queryIdx].pt);
-                refPts.push_back(refKeyPts[knnMatches[i][0].trainIdx].pt);
+                framePoints.push_back(frameKeyPts[knnMatches[i][0].queryIdx].pt);
+                refPoints.push_back(refKeyPts[knnMatches[i][0].trainIdx].pt);
             }
         }
     }
