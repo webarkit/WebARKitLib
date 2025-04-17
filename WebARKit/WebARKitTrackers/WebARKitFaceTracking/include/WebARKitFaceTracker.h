8852413aa050f0a64aeae61a05f118e7721871b3
Walter Perdan <github@kalwaltart.it>
2025-04-17 16:06:11 +0200

    refactor: improve code readability and organization in WebARKitFaceTracker

--- WebARKit/WebARKitTrackers/WebARKitFaceTracking/include/WebARKitFaceTracker.h
+++ WebARKit/WebARKitTrackers/WebARKitFaceTracking/include/WebARKitFaceTracker.h
@@ -1,71 +1,67 @@
 #ifndef WEBARKITFACETRACKER_H
 #define WEBARKITFACETRACKER_H
 
-#define DLIB_NO_GUI_SUPPORT 1
+//#define DLIB_NO_GUI_SUPPORT 1
 
 #include <cmath>
-//#include <dlib/gui_widgets.h>
+#include <iostream>
+#include <string>
+
 #include <dlib/image_processing.h>
 #include <dlib/image_processing/frontal_face_detector.h>
-//#include <dlib/image_processing/render_face_detections.h>
 #include <dlib/opencv.h>
-#include <iostream>
-#include <opencv2/core.hpp>    // Basic OpenCV structures (cv::Mat, Scalar)
+
+#include <opencv2/core.hpp>
 #include <opencv2/core/types_c.h>
-//#include <opencv2/highgui.hpp> // OpenCV window I/O
-//#include <opencv2/imgproc.hpp> // Gaussian Blur
 #include <opencv2/video.hpp>
-//#include <opencv2/videoio.hpp>
-#include <string>
+
+#include <WebARKit/WebARKitLog.h>
 
 constexpr int RESIZE_HEIGHT = 360;
 
 namespace webarkit {
+
 class WebARKitFaceTracker {
 public:
   WebARKitFaceTracker();
   ~WebARKitFaceTracker();
 
   void init(const std::string &model_path);
-  
   void track(const cv::Mat &image);
 
 private:
-  double interEyeDistance(dlib::full_object_detection& shape);
+  double interEyeDistance(dlib::full_object_detection &shape);
   bool preTrack(const cv::Mat &image);
 
+  // Optical flow parameters
   cv::TermCriteria termcrit;
   cv::Size winSize;
+  int maxLevel;
+
+  // Eye distance and stabilization parameters
   double eyeDistance, dotRadius, sigma;
   bool eyeDistanceNotCalculated;
-  int maxLevel;
-  std::vector<uchar> status;
 
-  std::vector<float> err;
-
-  double fps;
-
-  // Space for current frame, previous frame, and the grayscale versions.
+  // Frame and image data
   cv::Mat im, imSmall, imPrev, imGray, imGrayPrev;
-
-  // Vector of images for storing image pyramids for optical flow
   std::vector<cv::Mat> imGrayPyr, imGrayPrevPyr;
 
+  // Face detection and landmark detection
   dlib::frontal_face_detector detector;
   dlib::shape_predictor landmarkDetector;
-
-  // Vector to store face rectangles
   std::vector<dlib::rectangle> faces;
 
-  // Space for landmark points
+  // Landmark points
   std::vector<cv::Point2f> points, pointsPrev, pointsDetectedCur, pointsDetectedPrev;
 
-  // First frame is handled differently.
+  // Optical flow status and error
+  std::vector<uchar> status;
+  std::vector<float> err;
+
+  // Flags and settings
   bool isFirstFrame;
-
-  // Show stabilized video flag
   bool showStabilized;
-
+  double fps;
   cv::Size prevSize;
 };
 
